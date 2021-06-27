2ada10dc62650bdcb520fae166dd6168319c8e18
miyakowork <765934806@qq.com>
2021-06-27 15:50:57 +0800

    v1.4.1

--- notepress-service/src/main/java/me/wuwenbin/notepress/service/mapper/OauthMapper.java
+++ notepress-service/src/main/java/me/wuwenbin/notepress/service/mapper/OauthMapper.java
@@ -1,5 +1,6 @@
 package me.wuwenbin.notepress.service.mapper;
 
+import com.xkcoding.http.config.HttpConfig;
 import me.wuwenbin.notepress.api.annotation.MybatisMapper;
 import me.wuwenbin.notepress.api.model.entity.system.Oauth;
 import me.wuwenbin.notepress.api.query.OauthQuery;
@@ -10,6 +11,9 @@ import me.zhyd.oauth.request.AuthGiteeRequest;
 import me.zhyd.oauth.request.AuthGithubRequest;
 import me.zhyd.oauth.request.AuthQqRequest;
 import me.zhyd.oauth.request.AuthRequest;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 /**
  * created by Wuwenbin on 2019/11/28 at 9:13 上午
@@ -34,9 +38,17 @@ public interface OauthMapper extends NotePressMapper<Oauth> {
         switch (type) {
             case "github":
                 authRequest = new AuthGithubRequest(AuthConfig.builder()
+                        .ignoreCheckState(true)
                         .clientId(oauth.getClientId())
                         .clientSecret(oauth.getClientSecret())
                         .redirectUri(oauth.getRedirectUri())
+                        //github可能会抽风，需要配置代理
+//                        .httpConfig(HttpConfig.builder()
+//                                // Http 请求超时时间
+//                                .timeout(15000)
+//                                // host 和 port 请修改为开发环境的参数
+//                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7890)))
+//                                .build())
                         .build());
                 break;
             case "qq":
